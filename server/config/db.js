340b25640758d79eddce1f24c607ea921796ec0c
Aman singh baghel <141499645+itsaman22@users.noreply.github.com>
2025-08-11 19:26:56 +0530

    ssl tls

--- server/config/db.js
+++ server/config/db.js
@@ -6,7 +6,13 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      ssl: true,
+      tls: true,
+      tlsAllowInvalidCertificates: true // Only use this in development
+    });
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
