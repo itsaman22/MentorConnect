861ad70ed7f4c3fda7e4574ebbd650d13af20ab5
Aman singh baghel <141499645+itsaman22@users.noreply.github.com>
2025-08-12 22:45:23 +0530

    Refactor login functionality and improve error handling; add Vercel configuration

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import process from 'process'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,5 +12,8 @@ export default defineConfig({
     commonjsOptions: {
       transformMixedEsModules: true
     }
+  },
+  define: {
+    'import.meta.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL)
   }
 })
