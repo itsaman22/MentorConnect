d1ef7b11a9da8753d38b89c58304a9fe43b09bbf
Aman singh baghel <141499645+itsaman22@users.noreply.github.com>
2025-08-14 12:37:56 +0530

    user name not showing isue

--- server/server.js
+++ server/server.js
@@ -12,6 +12,22 @@ const app = express();
 // Connect to MongoDB
 connectDB();
 
+// Simple CORS middleware
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://mentor-connect-mauve.vercel.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Cache-Control');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  
+  // Handle preflight requests
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+    return;
+  }
+  
+  next();
+});
+
 // Configure CORS
 const corsOptions = {
   origin: function (origin, callback) {
