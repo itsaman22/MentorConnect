f8220ba1787398e3cf414f2bd121a6564d9e76fa
Aman singh baghel <141499645+itsaman22@users.noreply.github.com>
2025-08-14 12:32:21 +0530

    user name not showing isue

--- server/server.js
+++ server/server.js
@@ -16,10 +16,8 @@ connectDB();
 const corsOptions = {
   origin: [
     'http://localhost:5173',
-    'http://localhost:5174',
-    'http://localhost:5175',
-    'http://localhost:5176',
     'https://mentor-connect-mauve.vercel.app'
+      *
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'Cache-Control'],
